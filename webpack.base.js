d56b9f5db07fdf24ec2bc4f18afc8f0aaac66c11
TillMac <danny890308@gmail.com>
2023-05-23 12:26:30 +0800

    chore: remove [contenthash] to avoid cache

--- webpack.base.js
+++ webpack.base.js
@@ -6,7 +6,7 @@ module.exports = {
 	entry: './src/index.tsx',
 	output: {
 		path: path.resolve(__dirname, 'dist'),
-		filename: 'bundle.[contenthash].js',
+		filename: 'bundle.js',
 		clean: true,
 	},
 	module: {
